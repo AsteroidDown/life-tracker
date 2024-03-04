5eadc3cff098ea7405d76de1c96acef3ea7fd94b
AsteroidDown <jasper@stahmer.ca>
2024-03-04 14:03:33 -0500

    No tracker

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"version.json": "4ac40019c59b75ec57e872c2fe628c70",
-"index.html": "f844ab00d46b22991e615615e9d7c668",
-"/": "f844ab00d46b22991e615615e9d7c668",
-"main.dart.js": "f3afcde5758d91b69a5714af51338936",
+"index.html": "94338a3dd0121cea3452a1ec68b9e38d",
+"/": "94338a3dd0121cea3452a1ec68b9e38d",
+"main.dart.js": "1482e1288acface556303c8b2de16d94",
 "flutter.js": "c71a09214cb6f5f8996a531350400a9a",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
